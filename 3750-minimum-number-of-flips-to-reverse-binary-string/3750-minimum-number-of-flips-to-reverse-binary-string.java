73ff426562e10332431217d454aa5250f529e70e
Bharath_1608 <nagumallibharathkumar@gmail.com>
2026-06-01 22:02:44 +0530

    Time: 3 ms (34.38%), Space: 42.9 MB (45.52%) - LeetHub

---
+++ 3750-minimum-number-of-flips-to-reverse-binary-string/3750-minimum-number-of-flips-to-reverse-binary-string.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minimumFlips(int n) {
+        String str=Integer.toBinaryString(n);
+        String rev=new StringBuilder(str).reverse().toString();
+        int mis=0;
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)!=rev.charAt(i)){
+                mis++;
+            }
+        }
+        int min=Math.min(mis,str.length()-mis);
+        return mis;
+    }
+}
