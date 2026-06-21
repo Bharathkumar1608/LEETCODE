b3f7cb986c46ba1800098b3244bdd3c88b5c441e
Bharath_1608 <nagumallibharathkumar@gmail.com>
2026-06-21 10:32:18 +0530

    Time: 36 ms (77.33%), Space: 84.2 MB (8.76%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
@@ -7,6 +7,9 @@ class Solution {
                 coins-=costs[i];
                 count++;
             }
+            else{
+                break;
+            }
         }
         return count;
     }
