3cf95cb75244fc8ce5e438694d5b3fc3b11e3ef8
Bharath_1608 <nagumallibharathkumar@gmail.com>
2026-06-24 10:17:44 +0530

    Time: 5 ms (0.36%), Space: 44.5 MB (5.22%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -5,8 +5,7 @@ class Solution {
             arr[i]=nums[i];
         }
         Arrays.sort(arr);
-        System.out.println(Arrays.toString(arr));
-        System.out.println(Arrays.toString(nums));
+
         int num=arr[nums.length-1];
         for(int i=0;i<nums.length;i++){
             if(nums[i]==num){
