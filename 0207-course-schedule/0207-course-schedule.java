db119d8155a7b8ced30c4d03b7ee3c63bd1791b4
Bharath_1608 <nagumallibharathkumar@gmail.com>
2026-07-21 23:03:21 +0530

    Time: 6 ms (72.05%), Space: 47 MB (65.26%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,32 +1,33 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         int[] indeg=new int[numCourses];
-        ArrayList<ArrayList<Integer>> arr=new ArrayList<>();
+        ArrayList<ArrayList<Integer>> list=new ArrayList<>();
         for(int i=0;i<numCourses;i++){
-            arr.add(new ArrayList<>());
+            list.add(new ArrayList<>());
         }
-        for(int[] p: prerequisites){
-            arr.get(p[1]).add(p[0]);
+        for(int[] p:prerequisites){
+            list.get(p[1]).add(p[0]);
             indeg[p[0]]++;
         }
+        int count=0;
         Queue<Integer> que=new LinkedList<>();
-        for(int i=0;i<numCourses;i++){
+
+        for(int i=0;i<indeg.length;i++){
             if(indeg[i]==0){
                 que.add(i);
             }
-        }
-        int count=0;
+        } 
         while(!que.isEmpty()){
             int cur=que.poll();
             count++;
-            for(int i:arr.get(cur)){
+            for(int i:list.get(cur)){
                 indeg[i]--;
                 if(indeg[i]==0){
                     que.add(i);
                 }
             }
         }
-        if(count==numCourses){
+        if(numCourses==count){
             return true;
         }
         return false;
