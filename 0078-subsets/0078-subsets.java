81c56396761502dc3e80feb16bf12f336df8852c
Bharath_1608 <nagumallibharathkumar@gmail.com>
2026-08-05 23:19:42 +0530

    Time: 1 ms (86.72%), Space: 44.1 MB (57.87%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,19 +1,19 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> bucket=new ArrayList<>();
-        int n=nums.length;
-        int comb=1<<n;  // =Math.pow(2,n);
+        List<List<Integer>> list=new ArrayList<>();
 
-        for(int i=0;i<comb;i++){
-            List<Integer> list=new ArrayList<>();
+        rec(0,nums,list,new ArrayList<>());
 
-            for(int j=0;j<n;j++){
-                if((i&(1<<j))!=0){
-                    list.add(nums[j]);
-                }
-            }
-            bucket.add(list);
+        return list;
+    }
+    static void rec(int ind,int[] nums,List<List<Integer>> list,ArrayList<Integer> arr){
+        if(ind==nums.length){
+            list.add(new ArrayList<>(arr));
+            return;
         }
-        return bucket;
+        arr.add(nums[ind]);
+        rec(ind+1,nums,list,arr);
+        arr.remove(arr.size()-1);
+        rec(ind+1,nums,list,arr);
     }
 }
