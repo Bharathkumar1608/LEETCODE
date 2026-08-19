9f7b453ad82f4c29ccbccec236b86c4663d6ed10
Bharath_1608 <nagumallibharathkumar@gmail.com>
2026-08-19 12:41:01 +0530

    Time: 5 ms (99.8%), Space: 43.1 MB (71.41%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,24 +1,12 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        int[][] dp=new int[coins.length][amount+1];
-        for(int i=0;i<coins.length;i++){
-            Arrays.fill(dp[i],-1);
+        int[] dp=new int[amount+1];
+        dp[0]=1;
+        for(int coin:coins){
+            for(int i=coin;i<=amount;i++){
+                dp[i]+=dp[i-coin];
+            }
         }
-        return rec(0,coins,amount,dp);
-    }
-    static int rec(int ind,int[] arr,int tar,int[][] dp){
-        if(tar==0){
-            return 1;
-        }
-        if(ind==arr.length){
-            return 0;
-        }
-        if(tar<0){
-            return 0;
-        }
-        if(dp[ind][tar]!=-1){
-            return dp[ind][tar];
-        }
-        return dp[ind][tar]=(rec(ind,arr,tar-arr[ind],dp)+rec(ind+1,arr,tar,dp));
+        return dp[amount];
     }
 }
