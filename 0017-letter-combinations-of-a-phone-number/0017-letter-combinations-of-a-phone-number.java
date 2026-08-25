23c227e3b670029d59156339ece9a566f6840fc2
Bharath_1608 <nagumallibharathkumar@gmail.com>
2026-08-25 17:09:00 +0530

    Time: 0 ms (100%), Space: 43.3 MB (78.34%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,19 +1,19 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
-       List<String> list=new ArrayList<>();
-       String[] d={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-       rec(0,list,d,digits,new StringBuilder());
-       return list;
+        List<String> list=new ArrayList<>();
+        String[] l={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        backtrack(0,digits,l,list,new StringBuilder());
+        return list;
     }
-    static void rec(int ind,List<String> list,String[] d,String digits,StringBuilder sb){
-        if(ind==digits.length()){
+    static void backtrack(int ind,String word,String[] arr,List<String> list,StringBuilder sb){
+        if(ind==word.length()){
             list.add(sb.toString());
             return;
         }
-        String dum=d[digits.charAt(ind)-'0'];
-        for(int i=0;i<dum.length();i++){
-            sb.append(dum.charAt(i));
-            rec(ind+1,list,d,digits,sb);
+        String w=arr[Integer.parseInt(String.valueOf(word.charAt(ind)))];
+        for(int i=0;i<w.length();i++){
+            sb.append(w.charAt(i));
+            backtrack(ind+1,word,arr,list,sb);
             sb.deleteCharAt(sb.length()-1);
         }
     }
