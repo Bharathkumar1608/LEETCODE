dfd3a4d3446cf1bd092cc4c68e106d27c8299e13
Bharath_1608 <nagumallibharathkumar@gmail.com>
2026-09-04 23:40:47 +0530

    Time: 2101 ms (9.68%), Space: 0B (100%) - LeetHub

---
+++ 0607-sales-person/0607-sales-person.sql
@@ -0,0 +1,6 @@
+SELECT s.name FROM SalesPerson s
+Where s.name not in
+(SELECT s.name FROM SalesPerson s
+LEFT JOIN Orders o ON s.sales_id=o.sales_id
+LEFT JOIN Company c ON o.com_id=c.com_id
+WHERE c.name="RED")
