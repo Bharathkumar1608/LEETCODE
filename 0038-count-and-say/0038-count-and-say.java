dc267bfda3619e97d3246dbb97449407bca57042
Bharath_1608 <nagumallibharathkumar@gmail.com>
2026-09-07 20:24:04 +0530

    Time: 2 ms (99.7%), Space: 43 MB (55.15%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.java
+++ 0038-count-and-say/0038-count-and-say.java
@@ -1,29 +1,28 @@
 class Solution {
     public String countAndSay(int n) {
-        if(n==1){
-            return "1";
-        }
-        return RLE(countAndSay(n-1));
+       if(n==1){
+        return "1";
+       }
+       return RLE(countAndSay(n-1));
     }
     static String RLE(String str){
         StringBuilder sb=new StringBuilder();
-        char[] arr=str.toCharArray();
+        char[] carr=str.toCharArray();
         int c=0;
-        char cur=str.charAt(0);
-        for(char ch:arr){
-            if(cur==ch){
+        char cur=carr[0];
+        for(char ch:carr){
+            if(ch==cur){
                 c++;
             }
             else{
                 sb.append(c);
                 sb.append(cur);
-                cur=ch;
                 c=1;
+                cur=ch;
             }
         }
         sb.append(c);
         sb.append(cur);
         return sb.toString();
-
     }
 }
