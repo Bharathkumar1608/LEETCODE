5f4c69d7265c95a27a072feca93b87bbe38c76a5
Bharath_1608 <nagumallibharathkumar@gmail.com>
2026-09-07 22:28:58 +0530

    Time: 2 ms (100%), Space: 47 MB (70.54%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -8,7 +8,10 @@ class Solution {
         }
         return rec(word1,word2,m-1,n-1,dp);
     }
-    static int rec(String s1,String s2,int i,int j,int[][] dp){
+    static int rec(String s,String t,int i,int j,int[][] dp){
+        if(i<0 &&j<0){
+            return 0;
+        }
         if(i<0){
             return j+1;
         }
@@ -18,9 +21,9 @@ class Solution {
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
-        if(s1.charAt(i)==s2.charAt(j)){
-            return dp[i][j]=rec(s1,s2,i-1,j-1,dp);
+        if(s.charAt(i)==t.charAt(j)){
+            return dp[i][j]=rec(s,t,i-1,j-1,dp);
         }
-        return dp[i][j]=Math.min(1+rec(s1,s2,i,j-1,dp),Math.min(1+rec(s1,s2,i-1,j,dp),1+rec(s1,s2,i-1,j-1,dp)));
+        return dp[i][j]=Math.min(1+rec(s,t,i-1,j,dp),Math.min(1+rec(s,t,i-1,j-1,dp),1+rec(s,t,i,j-1,dp)));
     }
 }
